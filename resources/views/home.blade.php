ada6f8c948217c2d557768bd4253d456497974e3
MicheleBenaco <benaco.michele@yahoo.it>
2022-12-15 16:17:52 +0100

    base

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+{{-- Questo mette il layouts di base uguale per tutti  --}}
+@extends('layouts.app')
+{{-- mentre con questo stotisco allo yield con il codice dentro questa pagina --}}
+@section('content ')
+    <h1>Home page</h1>
+@endsection
